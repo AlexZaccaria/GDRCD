5fe419fb966fcb542c42556f6d863c4a2de54b62
Marco Orlandin <marco.orlandin@sixthcontinent.com>
2019-12-30 17:52:28 +0100

    commented session_id in response as injection

--- api/json_responder.php
+++ api/json_responder.php
@@ -4,7 +4,8 @@ class JsonResponder {
 
     function responde($response) {
         header('Content-Type: application/json');
-        $response['session_id'] = session_id();
+        // uncomment the next line if you're looking for session_id injection over responses
+        // $response['session_id'] = session_id();
         $encoded_respone = serialize($response);
         echo json_encode($response);
     }
